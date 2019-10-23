430528c8444074fa94504b91c45c8653fd7d384c
zzorin <ilya-zorin@yandex.ru>
2019-10-23 12:22:24 +0700

    completed chapter 4

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+describe Contact do
+  it 'has a valid factory' do
+    expect(build(:contact)).to be_valid
+  end
+  it 'has three phone numbers' do
+    expect(create(:contact).phones.count).to eq 3
+  end
+  it 'is valid with a firstname, lastname and email' do
+    expect(create(:contact)).to be_valid
+  end
+  it 'is invalid without a firstname' do
+    contact = build(:contact, firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to include("can't be blank")
+  end
+  it 'is invalid without a lastname' do
+    contact = build(:contact, lastname: nil)
+    contact.valid?
+    expect(contact.errors[:lastname]).to include("can't be blank")
+  end
+  it 'is invalid without an email address' do
+    contact = build(:contact, email: nil)
+    contact.valid?
+    expect(contact.errors[:email]).to include("can't be blank")
+  end
+  it 'is invalid with a duplicate email address' do
+    create(:contact, email: 'tester@example.com')
+    contact = build(:contact, email: 'tester@example.com')
+    contact.valid?
+    expect(contact.errors[:email]).to include("has already been taken")
+  end
+  it "returns a contact's full name as a string" do
+    contact = build(:contact,
+      firstname: "Jane",
+      lastname: "Smith"
+    )
+    expect(contact.name).to eq 'Jane Smith'
+  end
+
+  describe "filter last name by letter" do
+    before :each do
+      @smith = create(:contact,
+        firstname: 'John',
+        lastname: 'Smith'
+      )
+      @jones = create(:contact,
+        firstname: 'Tim',
+        lastname: 'Jones'
+      )
+      @johnson = create(:contact,
+        firstname: 'John',
+        lastname: 'Johnson'
+      )
+    end
+    context "with matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter('J')).to eq [@johnson, @jones]
+      end
+    end
+    context "with non-matching letters" do
+      it "omit results that do not match" do
+        expect(Contact.by_letter("J")).not_to include @smith
+      end
+    end
+  end
+
+end
